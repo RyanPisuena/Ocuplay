a69dc023a126aa5d14e7d4f4c3fc8651bb54d9be
NinjaCatBob <ace.g.fig@gmail.com>
2018-09-28 16:22:27 -0700

    Added .pro file

---
+++ graphicDesignProject/Project1/Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	const int SIZE = 100;
+	int r[SIZE][SIZE], g[SIZE][SIZE], b[SIZE][SIZE];
+	//This is the red pixel
+	for (int i = 0; i < SIZE; i++) {
+		
+		for (int j = 0; j < SIZE; j++) {
+			r[i][j] = 255;
+			g[i][j] = 0;
+			b[i][j] = 0;
+		}
+
+	}
+	//This is the green pixel
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			r[i][j] = 0;
+			g[i][j] = 255;
+			b[i][j] = 0;
+		}
+
+	}
+	//This is the blue pixel
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			r[i][j] = 0;
+			g[i][j] = 0;
+			b[i][j] = 255;
+		}
+
+	}
+	return 0;
+}
